639693d8884216a38aa96c2dbfe34a698b5b4224
江南一点雨 <wangsong0210@gmail.com>
2020-05-25 19:39:41 +0800

    Squashed 'vmall-seller-auth/' content from commit f00d4ec
    git-subtree-dir: vmall-seller-auth
    git-subtree-split: f00d4ec56c1a0743afa836a6a487fc70b727d9d8

---
+++ src/test/java/org/javaboy/vmall/seller/auth/VmallSellerAuthApplicationTests.java
@@ -0,0 +1,15 @@
+package org.javaboy.vmall.seller.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootTest
+class VmallSellerAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("   v  h  r".replaceAll("\\s*",""));
+    }
+
+}
