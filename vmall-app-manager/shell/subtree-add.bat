2b9b5e70d59d495aabe3b1f19bd1899202188331
muggle <isocket@outlook.com>
2020-05-25 18:13:26 +0800

    git公共仓库

--- vmall-app-manager/shell/subtree-add.bat
+++ vmall-app-manager/shell/subtree-add.bat
@@ -1,3 +1,3 @@
 cd ..
-
-git subtree add --prefix=src/main/java/org/javaboy/appmanager/common origin common-0.1.0.Alpha --squash
+cd ..
+git subtree add --prefix=vmall-app-manager/src/main/java/org/javaboy/appmanager/common origin common-0.1.0.Alpha --squash
