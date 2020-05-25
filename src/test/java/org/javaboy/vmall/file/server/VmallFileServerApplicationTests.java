81a2dcb99ab728416d50c053049c6dde215251e5
江南一点雨 <wangsong0210@gmail.com>
2020-05-25 19:42:39 +0800

    Squashed 'vmall-file-server/' content from commit 1b5d4b3
    git-subtree-dir: vmall-file-server
    git-subtree-split: 1b5d4b3efd42db09387b28038c6a977998af9a04

---
+++ src/test/java/org/javaboy/vmall/file/server/VmallFileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaboy.vmall.file.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmallFileServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
